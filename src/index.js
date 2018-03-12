195f974784f9b8204d705898369b69e6429b4825
Sankarsan Kampa <me@sankarsankampa.com>
2018-03-12 13:35:37 +0000

    Implemented service worker

--- src/index.js
+++ src/index.js
@@ -2,5 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './Components/App';
+import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('_bastion-root'));
+registerServiceWorker();
