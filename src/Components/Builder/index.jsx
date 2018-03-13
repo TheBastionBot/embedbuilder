66136b9d6d9d9051c3527224e0c7821c50629765
Sankarsan Kampa <me@sankarsankampa.com>
2018-03-13 05:53:48 +0000

    Added base Builder & Output components

---
+++ src/Components/Builder/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class Builder extends React.Component {
+  render() {
+    return(
+      <div className = "cell">
+      </div>
+    );
+  }
+}
+
+export default Builder;
---
+++ src/Components/Output/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class Output extends React.Component {
+  render() {
+    return(
+      <div className = "cell">
+      </div>
+    );
+  }
+}
+
+export default Output;
