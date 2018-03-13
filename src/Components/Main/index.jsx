8cb1d758773ce074e5bcb899d4f15e4112edff11
Sankarsan Kampa <me@sankarsankampa.com>
2018-03-13 05:54:42 +0000

    Integrate Builder & Output component in Main

--- src/Components/Main/index.jsx
+++ src/Components/Main/index.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import Builder from '../Builder';
+import Output from '../Output';
 
 class Main extends React.Component {
   render() {
     return (
       <main>
+        <Builder />
+        <Output />
       </main>
     );
   }
