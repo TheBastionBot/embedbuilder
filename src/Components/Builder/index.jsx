d3c86be38db72e023d17861cb43825305f429edb
Sankarsan Kampa <me@sankarsankampa.com>
2018-03-13 06:11:24 +0000

    Updated Builder structure

--- src/Components/Builder/index.jsx
+++ src/Components/Builder/index.jsx
@@ -3,7 +3,17 @@ import React from 'react';
 class Builder extends React.Component {
   render() {
     return(
-      <div className = "cell">
+      <div className="cell">
+        <div id="builder-container">
+          <form id="embed-builder">
+            <div className="embed">
+              <div className="embed-content">
+                <div className="embed-content-inner">
+                </div>
+              </div>
+            </div>
+          </form>
+        </div>
       </div>
     );
   }
