f9faa01964198a0b32394a364fd5262904df67c6
Sankarsan Kampa <19631364+k3rn31p4nic@users.noreply.github.com>
2020-06-11 12:22:32 +0530

    Updates

---
+++ precache-manifest.70d79ec6acd28c787e6926e2c2886fee.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a3bf7c90b039ec9a2da6cb2ecd783216",
+    "url": "/embedbuilder/index.html"
+  },
+  {
+    "revision": "8eef2c87edc1eaf450f6",
+    "url": "/embedbuilder/static/css/main.d24b7906.chunk.css"
+  },
+  {
+    "revision": "1e5244336f84a54cf0fc",
+    "url": "/embedbuilder/static/js/2.d1cd0897.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/embedbuilder/static/js/2.d1cd0897.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8eef2c87edc1eaf450f6",
+    "url": "/embedbuilder/static/js/main.043bbda4.chunk.js"
+  },
+  {
+    "revision": "c50bcd78f5d006e84b55",
+    "url": "/embedbuilder/static/js/runtime-main.b5bfa4d3.js"
+  }
+]);
